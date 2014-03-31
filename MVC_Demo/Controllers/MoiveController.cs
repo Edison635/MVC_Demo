b3a7ecb44f0c86a4f0b1a56908f08e320d75e737
Edison635 <Edison635@Gmail.com>
2014-03-31 17:15:50 +0800

    Init MVC_Demo

---
+++ MVC_Demo/Controllers/MoiveController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common.CommandTrees;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Demo.Models;
+
+namespace MVC_Demo.Controllers
+{
+    public class MoiveController : Controller
+    {
+        private MoiveDbContext db = new MoiveDbContext();
+
+        //
+        // GET: /Moive/
+
+        public ActionResult Index()
+        {
+            return View(db.Moives.ToList());
+        }
+
+        //
+        // GET: /Moive/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Moive moive = db.Moives.Find(id);
+            if (moive == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moive);
+        }
+
+        //
+        // GET: /Moive/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Moive/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Moive moive)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Moives.Add(moive);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(moive);
+        }
+
+        //
+        // GET: /Moive/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Moive moive = db.Moives.Find(id);
+            if (moive == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moive);
+        }
+
+        //
+        // POST: /Moive/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Moive moive)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(moive).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(moive);
+        }
+
+        //
+        // GET: /Moive/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Moive moive = db.Moives.Find(id);
+            if (moive == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moive);
+        }
+
+        //
+        // POST: /Moive/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Moive moive = db.Moives.Find(id);
+            db.Moives.Remove(moive);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult SearchIndex(string moiveGeren,string SearchString)
+        {
+            var geren = new List<string>();
+            var querygeren = from p in db.Moives
+                orderby p.Genre
+                select p.Genre;
+            geren.AddRange(querygeren.Distinct());
+
+            ViewBag.moiveGeren = new SelectList(geren);
+
+            var moives = from m in db.Moives
+                select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                moives = moives.Where(c => c.Title.Contains(SearchString));
+            }
+            if (string.IsNullOrEmpty(moiveGeren))
+                return View(moives);
+            return View(moives.Where(c => c.Genre == moiveGeren));
+            
+        }
+    }
+}
---
+++ MVC_Demo/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo.Models
+{
+    public class Moive
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseTime { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+
+       
+
+    }
+
+    public class MoiveDbContext:DbContext
+    {
+        public DbSet<Moive> Moives { get; set; }
+    }
+
+
+
+}
